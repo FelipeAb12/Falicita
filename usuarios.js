fb6db4ccd3fd7fd413ea7cded034b32187bd0e2e
FelipeAb12 <135764751+FelipeAb12@users.noreply.github.com>
2023-06-14 11:03:08 -0300

    Arquivos
    Adicionei os arquivos

---
+++ usuarios.js
@@ -0,0 +1,14 @@
+const usuarios = [
+    {
+        login: 'Felipe',
+        senha: 'Felipe'
+    },
+    {
+        login: 'Lucas',
+        senha: 'Lucas'
+    },
+    {
+        login: 'admin',
+        senha: 'admin'
+    }
+];
